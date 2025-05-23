9d0e0b55ca6a2aeff7ba264a1081dfe42463653c
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-05-23 12:30:39 +0200

    Expand playingGrid to validate winning conditions

--- script.js
+++ script.js
@@ -2,13 +2,79 @@ const playingGrid = (function() {
   const grid = [[null, null, null],
                 [null, null, null],
                 [null, null, null]];
-  const playOne = (i, j) => { grid[i][j] = 1; };
-  const playTwo = (i, j) => { grid[i][j] = 2; };
-  const getSquare = (i, j) => grid[i][j];
+
+  function assert(i, j) {
+    console.assert(
+      typeof i === "number" &&
+      i >= 0 && i <= 2
+    );
+
+    if (j !== undefined) console.assert(
+      typeof j === "number" &&
+      j >= 0 && j <= 2
+    );
+  }
+
+  const playOne = (i, j) => {
+    assert(i, j);
+    grid[i][j] = 1;
+  };
+
+  const playTwo = (i, j) => {
+    assert(i, j);
+    grid[i][j] = 2;
+  };
+
+  const getSquare = (i, j) => {
+    assert(i, j);
+    return grid[i][j];
+  };
+
   const isEmpty = (i, j) => {
-    if (grid[i][j] === null) return true;
+    assert(i, j);
+    return grid[i][j] === null;
+  };
+
+  const rowWins = (row) => {
+    assert(row);
+    if (
+      !isEmpty(row, 0) &&
+      grid[row][0] === grid[row][1] &&
+      grid[row][0] === grid[row][2]
+    ) return grid[row][0];
     return false;
   };
-  return { playOne, playTwo, getSquare, isEmpty };
+
+  const colWins = (col) => {
+    assert(col);
+    if (
+      !isEmpty(0, col) &&
+      grid[0][col] === grid[1][col] &&
+      grid[0][col] === grid[2][col]
+    ) return grid[0][col];
+    return false;
+  };
+
+  const diagWins = () => {
+    if (
+      grid[0][0] === grid[1][1] &&
+      grid[0][0] === grid[2][2]
+    ) return grid[0][0];
+    if (
+      grid[0][2] === grid[1][1] &&
+      grid[0][2] === grid[2][0]
+    ) return grid[0][2]
+    return false;
+  };
+
+  const reset = () => {
+    for (let i = 0; i <= 2; i++) {
+      for (let j = 0; j <= 2; j++) {
+        assert(i, j);
+        grid[i][j] = null;
+      }
+    }
+  }
+  return { playOne, playTwo, getSquare, isEmpty, rowWins, colWins, diagWins, reset };
 })();
 
