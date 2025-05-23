9a07365d5bf49dcc3dc9e122165fb1dba8ebe9f4
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-05-23 23:38:10 +0200

    Implement a Game object to control the game's DOM Game has various methods to generate the grid and to add a cross/circle SVG

--- script.js
+++ script.js
@@ -90,3 +90,75 @@ function Player(name, isAIOn) {
 
   return { isAI, getName, getWins, hasWon, resetWins };
 }
+
+const Game = (function() {
+  const _container = document.querySelector(".container");
+  let _isFilled = false;
+
+  function assert(...indices) {
+    for (const index of indices) {
+      console.assert(
+        typeof index === "number" &&
+        index >= 0 && index <= 2
+      );
+    }
+  }
+
+  const fillUpGrid = () => {
+    const _divTemplate = document.createElement("div");
+    for (let i = 0; i <= 8; i++) {
+      const _div = _divTemplate.cloneNode(true);
+      _container.appendChild(_div);
+    }
+    _isFilled = true;
+  };
+
+  const addCross = (row, col) => {
+    assert(row, col);
+
+    const _svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    _svg.setAttribute("viewBox", "0 0 10 10");
+
+    const _path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    _path.setAttribute("d",
+      "M 1 2 C 0 1 1 0 2 1 L 5 4 L 8 1 C 9 0 10 1 9 2 L 6 5 L 9 8 C 10 9 9 10 8 9 L 5 6 L 2 9 C 1 10 0 9 1 8 L 4 5 Z");
+    _svg.appendChild(_path);
+
+    const _nthCell = document.querySelector(`.container div:nth-child(${row * 3 + col + 1})`);
+    _nthCell.appendChild(_svg);
+  };
+
+  const addCircle = (row, col) => {
+    assert(row, col);
+
+    const _svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    _svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    _svg.setAttribute("viewBox", "0 0 10 10");
+
+    const _circleOut = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    _circleOut.setAttribute("cx", "5");
+    _circleOut.setAttribute("cy", "5");
+    _circleOut.setAttribute("r", `${6 - Math.sqrt(2)}`);
+    _circleOut.classList.add("out");
+    _svg.appendChild(_circleOut);
+
+    const _circleIn = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    _circleIn.setAttribute("cx", "5");
+    _circleIn.setAttribute("cy", "5");
+    _circleIn.setAttribute("r", `${6 - 2 * Math.sqrt(2)}`);
+    _circleIn.classList.add("in");
+    _svg.appendChild(_circleIn);
+
+    const _nthCell = document.querySelector(`.container div:nth-of-type(${row * 3 + col + 1})`);
+    _nthCell.appendChild(_svg);
+  };
+
+  return { fillUpGrid, addCross, addCircle };
+})();
+
+Game.fillUpGrid();
+
+Game.addCross(1, 1);
+Game.addCircle(0, 0);
+Game.addCross(0, 2);
+Game.addCircle(2, 0);
