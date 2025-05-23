42dcf44ddb866cae8684ab3a69e39e7e00090c92
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-05-23 14:45:31 +0200

    Refactor playingGrid object from 2D to 1D array 1D arrays are more efficient for both performance and game AIs

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const playingGrid = (function() {
-  const grid = [[null, null, null],
-                [null, null, null],
-                [null, null, null]];
+  const grid = new Array(9);
 
   function assert(...indices) {
     for (const index of indices) {
@@ -12,33 +10,33 @@ const playingGrid = (function() {
     }
   }
 
-  const playOne = (i, j) => {
-    assert(i, j);
-    grid[i][j] = 1;
+  const playOne = (row, col) => {
+    assert(row, col);
+    grid[row * 3 + col] = 1;
   };
 
-  const playTwo = (i, j) => {
-    assert(i, j);
-    grid[i][j] = 2;
+  const playTwo = (row, col) => {
+    assert(row, col);
+    grid[row * 3 + col] = 2;
   };
 
-  const getSquare = (i, j) => {
-    assert(i, j);
-    return grid[i][j];
+  const getSquare = (row, col) => {
+    assert(row, col);
+    return grid[row * 3 + col];
   };
 
-  const isEmpty = (i, j) => {
-    assert(i, j);
-    return grid[i][j] === null;
+  const isEmpty = (row, col) => {
+    assert(row, col);
+    return grid[row * 3 + col] === null;
   };
 
   const rowWins = (row) => {
     assert(row);
     if (
       !isEmpty(row, 0) &&
-      grid[row][0] === grid[row][1] &&
-      grid[row][0] === grid[row][2]
-    ) return grid[row][0];
+      grid[row * 3] === grid[row * 3 + 1] &&
+      grid[row * 3] === grid[row * 3 + 2]
+    ) return grid[row * 3];
     return false;
   };
 
@@ -46,30 +44,27 @@ const playingGrid = (function() {
     assert(col);
     if (
       !isEmpty(0, col) &&
-      grid[0][col] === grid[1][col] &&
-      grid[0][col] === grid[2][col]
-    ) return grid[0][col];
+      grid[col] === grid[3 + col] &&
+      grid[col] === grid[6 + col]
+    ) return grid[col];
     return false;
   };
 
   const diagWins = () => {
     if (
-      grid[0][0] === grid[1][1] &&
-      grid[0][0] === grid[2][2]
-    ) return grid[0][0];
+      grid[0] === grid[4] &&
+      grid[0] === grid[8]
+    ) return grid[0];
     if (
-      grid[0][2] === grid[1][1] &&
-      grid[0][2] === grid[2][0]
-    ) return grid[0][2]
+      grid[2] === grid[4] &&
+      grid[2] === grid[6]
+    ) return grid[2]
     return false;
   };
 
   const reset = () => {
-    for (let i = 0; i <= 2; i++) {
-      for (let j = 0; j <= 2; j++) {
-        assert(i, j);
-        grid[i][j] = null;
-      }
+    for (let index = 0; index <= 8; index++) {
+      grid[index] = null;
     }
   }
   return { playOne, playTwo, getSquare, isEmpty, rowWins, colWins, diagWins, reset };
